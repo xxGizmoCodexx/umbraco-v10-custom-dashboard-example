d4c48d60e4e409bec121d44fea1d756511f103ff
Arno Bornman <bornmana101@gmail.com>
2022-08-12 13:15:33 +0200

    Initial commit

---
+++ Config/AddRoutingRules.cs
@@ -0,0 +1,53 @@
+﻿using Umbraco.Cms.Web.Common.ApplicationBuilder;
+
+namespace DemoUmbraco.Config
+{
+    public static partial class UmbracoApplicationBuilderExtensions
+    {
+        public static IUmbracoEndpointBuilderContext UseCustomRoutingRules(this IUmbracoEndpointBuilderContext app)
+        {
+            if (!app.RuntimeState.UmbracoCanBoot())
+            {
+                return app;
+            }
+
+            //app.EndpointRouteBuilder.MapControllerRoute(
+            //               "vanilla-route",
+            //               "/vanilla/{action}/{id?}",
+            //               new { Controller = "Vanilla", Action = "Index" });
+
+            app.EndpointRouteBuilder.MapControllerRoute(
+                           "secure-route",
+                           "umbraco/backoffice/Plugins/Backend/Index",
+                           new { Controller = "Backend", Action = "Index" });
+
+
+            app.EndpointRouteBuilder.MapControllerRoute(
+                           "AdminDefault",
+                           "umbraco/backoffice/plugins/admin/unpublishedtutorials",
+                           new { Controller = "Admin", Action = "Index" });
+
+            //app.EndpointRouteBuilder.MapControllerRoute(
+            //              "XmlSitemap",
+            //              "sitemap",
+            //              new
+            //              {
+            //                  controller = "XmlSitemap",
+            //                  action = "Index",
+            //                  id = UrlParameter.Optional
+            //              });
+
+            //app.EndpointRouteBuilder.MapControllerRoute(
+            //               "RSS",
+            //               "rss",
+            //               new
+            //               {
+            //                   controller = "Rss",
+            //                   action = "Index",
+            //                   id = UrlParameter.Optional
+            //               });
+
+            return app;
+        }
+    }
+}
---
+++ Dashboards/UnpublishedTutorialsDashboard.cs
@@ -0,0 +1,26 @@
+﻿using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Dashboards;
+
+namespace DemoUmbraco.Dashboards
+{
+    public class RegisterUnpublishedTutorialsDashboard : IDashboard
+    {
+        public string[] Sections => new[] { Constants.Applications.Content, "myCustomSection" };
+
+        public IAccessRule[] AccessRules
+        {
+            get
+            {
+                var rules = new IAccessRule[]
+                {
+                    new AccessRule {Type = AccessRuleType.Grant, Value = Constants.Security.EditorGroupAlias}
+                };
+                return rules;
+            }
+        }
+
+        public string Alias => "UnpublishedTutorials";
+
+        public string View => "/umbraco/backoffice/Plugins/Admin/UnpublishedTutorials";
+    }
+}
